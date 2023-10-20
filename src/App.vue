838aeac9e4de4c47d0b573d9c83369c51719e387
Nathan <7703374+nathan@user.noreply.gitee.com>
2023-10-20 12:20:46 +0800

    Add a view component of nl-input testing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
     <div id="app">
-        <nl-div>
-            <nl-button @clicked="decreasePercentage">减小百分比</nl-button>
-            <nl-button @clicked="increasePercentage">增加百分比</nl-button>
-        </nl-div>
-        <nl-progress :percentage="percentage" />
-        <nl-div>
-            <nl-progress type="circle" :percentage="percentage" :scale="0.5" />
-            <nl-progress type="circle" :percentage="percentage" />
-            <nl-progress type="circle" :percentage="percentage" :scale="1.5" />
-        </nl-div>
+        <nl-input-test></nl-input-test>
     </div>
 </template>
 
@@ -19,19 +10,12 @@ import { ref } from "vue";
 /**
  * Import components
  */
-import NlDiv from "./components/NlDiv.vue";
-import NlProgress from "./components/NlProgress.vue";
-import NlButton from "./components/NlButton.vue";
+import NlInputTest from "./views/NlInputTest.vue";
 
 /**
  * Define refs
  */
-const percentage = ref(0);
-
-// setInterval(() => (percentage.value = (percentage.value + 1) % 101), 240);
-
-const increasePercentage = () => (percentage.value += 10);
-const decreasePercentage = () => (percentage.value -= 10);
+const inputValue = ref('');
 </script>
 
 <style scoped>
@@ -39,15 +23,5 @@ const decreasePercentage = () => (percentage.value -= 10);
     display: flex;
     flex-direction: column;
     gap: 16px;
-
-    &:deep(.nl-button--liked) {
-        border-color: pink;
-        background-color: pink;
-    }
-
-    &:deep(.nl-button--checked) {
-        border-color: rgb(192, 242, 255);
-        background-color: rgb(192, 242, 255);
-    }
 }
 </style>
---
+++ src/views/NlInputTest.vue
@@ -0,0 +1,48 @@
+<template>
+    <h3>nl-input</h3>
+    <nl-input v-model="inputValues.iv1" />
+    <nl-input v-model="inputValues.iv2" placeholder="Input something" />
+    <nl-input v-model="inputValues.iv3" icon="icon-search" placeholder="Search" />
+    <nl-input v-model="inputValues.iv4" prefix="Http://" suffix="/" />
+    <nl-input
+        v-model="inputValues.iv5"
+        type="password"
+        maxlength="16"
+        show-password
+        placeholder="Your password (maxlength 16)" />
+    <nl-input v-model="inputValues.iv6" disabled />
+    <nl-input v-model="inputValues.iv7" readonly />
+    <nl-input v-model="inputValues.iv8" prefix="#" clearable placeholder="Input with clear button" />
+    <nl-input v-model="inputValues.iv9" counter="word-limit" maxlength="32" placeholder="Input with word counter" />
+    <nl-input v-model="inputValues.iv9" counter="both" maxlength="32" placeholder="Input with word counter" />
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+/**
+ * Import components
+ */
+import NlInput from "../components/NlInput.vue";
+
+/**
+ * Define options
+ */
+defineOptions({ name: "NlInputTest" });
+
+/**
+ * Define reactives
+ */
+const inputValues = reactive({
+    iv1: "",
+    iv2: "",
+    iv3: "",
+    iv4: "",
+    iv5: "",
+    iv6: "Disabled input",
+    iv7: "Readonly input",
+    iv8: "",
+    iv9: "",
+});
+</script>
+.
