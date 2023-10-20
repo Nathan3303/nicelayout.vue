f4051aaf8c16c7ea93518e5d4eb944b85f0914c4
Nathan <7703374+nathan@user.noreply.gitee.com>
2023-10-20 21:00:54 +0800

    Optimize code and add option round-no-border to shape validator

--- src/components/validators.js
+++ src/components/validators.js
@@ -1,4 +1,4 @@
-import { isNumber, hasPercentageSign, hasPixelSign } from './utils'
+import { isNumber, hasPercentageSign, hasPixelSign } from "./utils";
 
 /**
  * Validating function for verifying prop width and height
@@ -6,10 +6,8 @@ import { isNumber, hasPercentageSign, hasPixelSign } from './utils'
  * @return {boolean}
  */
 export function validateWidthAndHeight(value) {
-    if (isNumber(value)) return value
-    return hasPercentageSign(value) || hasPixelSign(value)
-        ? value
-        : ['auto', 'max-content', 'min-content'].includes(value)
+    if (isNumber(value)) return value;
+    return hasPercentageSign(value) || hasPixelSign(value) ? value : ["auto", "max-content", "min-content"].includes(value);
 }
 
 /**
@@ -18,8 +16,8 @@ export function validateWidthAndHeight(value) {
  * @return {boolean}
  */
 export function validateUnit(value) {
-    if (isNumber(value)) return value
-    return hasPercentageSign(value) || hasPixelSign(value) ? value : false
+    if (isNumber(value)) return value;
+    return hasPercentageSign(value) || hasPixelSign(value) ? value : false;
 }
 
 /**
@@ -28,7 +26,7 @@ export function validateUnit(value) {
  * @return {boolean}
  */
 export function validateSize(size) {
-    return isNumber(size) ? size : ['small', 'normal', 'medium', 'large'].includes(size)
+    return isNumber(size) ? size : ["small", "normal", "medium", "large"].includes(size);
 }
 
 /**
@@ -37,7 +35,7 @@ export function validateSize(size) {
  * @return {boolean}
  */
 export function validateShape(shape) {
-    return ['round', 'square', 'no-border'].includes(shape)
+    return ["square", "round", "no-border", "round-no-border"].includes(shape);
 }
 
 /**
@@ -46,5 +44,5 @@ export function validateShape(shape) {
  * @return {boolean}
  */
 export function validateFit(fit) {
-    return ['fill', 'cover', 'contain', 'none', 'scale-down'].includes(fit)
+    return ["fill", "cover", "contain", "none", "scale-down"].includes(fit);
 }
