676e584b682bf315cf2ef7d4af55e29faaff138f
Nathan <7703374+nathan@user.noreply.gitee.com>
2023-11-03 19:39:57 +0800

    Component test

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <template>
     <div id="app">
-        <!-- <nl-input-test /> -->
+        <nl-input-test />
         <!-- <nl-textarea-test /> -->
         <!-- <nl-button-test /> -->
-        <nl-progress-test />
+        <!-- <nl-progress-test /> -->
         <!-- <nl-avatar-test /> -->
         <!-- <timeline-test /> -->
     </div>
 </template>
 
 <script setup>
-// import NlInputTest from "./views/NlInputTest.vue";
-// import NlTextareaTest from "./views/NlTextareaTest.vue";
+import NlInputTest from "./views/nl-input-test.vue";
 // import NlButtonTest from "./views/nl-button-test.vue";
-import NlProgressTest from "./views/nl-progress-test.vue";
+// import NlProgressTest from "./views/nl-progress-test.vue";
 // import NlAvatarTest from "./views/nl-avatar-test.vue";
 // import TimelineTest from "./views/TimelineTest.vue";
 </script>
 
 <style scoped>
 #app {
+
     box-sizing: border-box;
-
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr;
-
-    width: 100vw;
-    height: 100vh;
+    /* width: 100vw; */
+    /* height: 100vh; */
     padding: 12px;
 }
 </style>
