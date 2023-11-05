f30109295c29043f3ce32a546cb13bdbe3cabe91
Nathan33 <Lee1928@outlook.com>
2023-11-05 16:46:13 +0800

    Optimize component structure

---
+++ src/components/timeline/src/timeline.js
@@ -0,0 +1,89 @@
+import { ref, reactive } from "vue";
+
+/**
+ * Define props
+ */
+export const timelineProps = {
+    /**
+     * @description Data of timeline
+     */
+    lines: Array,
+    /**
+     * @description Scale of timeline
+     */
+    scale: {
+        type: Number,
+        default: 2,
+        validator: (v) => v >= 1 && v <= 6,
+    },
+    /**
+     * @description Update time (second)
+     */
+    updateTime: {
+        type: Number,
+        default: 0,
+        validator: (v) => v >= 0 && v <= 60,
+    },
+    /**
+     * @description Pointer text display state
+     */
+    showPointerText: Boolean,
+};
+
+/**
+ * Use timeline hook
+ * @returns {Object} actions
+ */
+export const useTimeline = (scale = 1) => {
+    const hoursGap = ref(scale * 60);
+    const width = ref(hoursGap.value * 24);
+    const scaleLevel = ref(parseInt(scale / 3) + 1);
+    const timeScaleGap = ref(hoursGap.value / scaleLevel.value);
+    const secondsPerPixel = ref(parseInt(60 / scale));
+    const updateTime = ref(1000 * secondsPerPixel.value);
+    const pointer = reactive({ left: 0, text: "0:00" });
+
+    /**
+     * Calculate css left value by time string
+     * @param {string} time time string (hh:mm:ss)
+     * @return {string} css left value (px)
+     */
+    function calcLeftValueByTimeString(timeString) {
+        if (typeof timeString !== "string") return;
+        const splitRes = timeString.split(":");
+        const h = splitRes[0] * hoursGap.value;
+        const m = splitRes[1] * (hoursGap.value / 60);
+        const s = (splitRes[2] || 0) / secondsPerPixel.value;
+        return parseInt(h + m + s + 24);
+    }
+
+    /**
+     * Update pointer left by time string
+     * @param {String} timeString time string (hh:mm:ss)
+     */
+    function updatePointerLeft(timeString) {
+        pointer.left = calcLeftValueByTimeString(timeString) - 2;
+    }
+
+    /**
+     * Update pointer text
+     * @param {String} timeString time string
+     */
+    function updatePointerText(timeString) {
+        pointer.text = timeString;
+    }
+
+    /**
+     * Return
+     */
+    return {
+        width,
+        scaleLevel,
+        timeScaleGap,
+        updateTime,
+        pointer,
+        calcLeftValueByTimeString,
+        updatePointerLeft,
+        updatePointerText,
+    };
+};
