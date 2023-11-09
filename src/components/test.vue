2eb061ec368e2d317c2383d45fefa51a39c182b4
Nathan33 <Lee1928@outlook.com>
2023-11-09 10:43:57 +0800

    Component test

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,45 +1,10 @@
+<script setup></script>
+
 <template>
     <nl-div>
-        <nl-input type="number" v-model="inputValue" clearable></nl-input>
-        <nl-button @click="clickHandler">Go</nl-button>
+        <nl-link href="/">
+            <nl-avatar src="/images/users/nathan.png"></nl-avatar>
+        </nl-link>
+        <nl-link href="http://www.baidu.com" theme="button" icon="icon-arrow-right"> www.baidu.com </nl-link>
     </nl-div>
 </template>
-
-<script setup>
-import { ref } from "vue";
-// import { NlDiv, NlInput, NlButton } from "@nice-layout/components";
-
-/**
- * Define refs
- */
-const inputValue = ref();
-
-/**
- * Go button click handler
- */
-function clickHandler() {
-    const totalPages = parseInt(inputValue.value);
-    const countResult = countTimesOf0To9(totalPages);
-    console.log(countResult);
-}
-
-/**
- * Count times of 0 to 9
- * @param {Number} totalPages total pages
- * @returns {Array} times of 0 to 9
- */
-function countTimesOf0To9(totalPages) {
-    let n,
-        result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for (let i = 1; i <= totalPages; i++) {
-        n = i;
-        while (n) {
-            result[n % 10] += 1;
-            n = parseInt(n / 10);
-        }
-    }
-    return result;
-}
-</script>
-
-<style scoped></style>
