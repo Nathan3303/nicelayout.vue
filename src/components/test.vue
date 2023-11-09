93ac601f6ffc675157bc34e34de9fb96c4b37c4f
Nathan33 <Lee1928@outlook.com>
2023-11-09 11:12:21 +0800

    Component test

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,10 +1,24 @@
-<script setup></script>
-
 <template>
-    <nl-div>
-        <nl-link href="/">
-            <nl-avatar src="/images/users/nathan.png"></nl-avatar>
-        </nl-link>
-        <nl-link href="http://www.baidu.com" theme="button" icon="icon-arrow-right"> www.baidu.com </nl-link>
-    </nl-div>
+    <div class="test">
+        <nl-div justify-content="space-between">
+            <nl-link href="/">
+                <nl-avatar src="/images/users/nathan.png"></nl-avatar>
+            </nl-link>
+            <nl-link href="http://www.baidu.com" theme="button" icon="icon-arrow-right"> www.baidu.com </nl-link>
+        </nl-div>
+        <nl-div vertical>
+            <nl-link href="/">
+                <nl-avatar src="/images/users/nathan.png"></nl-avatar>
+            </nl-link>
+            <nl-link href="http://www.baidu.com" theme="button" icon="icon-arrow-right"> www.baidu.com </nl-link>
+        </nl-div>
+    </div>
 </template>
+
+<style scoped>
+.test {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+}
+</style>
