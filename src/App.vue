b4beb97d5e8f696c8a7eb6d95f9737297b03519d
Nathan33 <Lee1928@outlook.com>
2023-11-11 15:54:21 +0800

    Test link

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="app">
         <test-avatar></test-avatar>
+        <test-link></test-link>
     </div>
 </template>
 
 <script setup>
-import testAvatar from './components/test-avatar.vue';
+import testAvatar from "./components/test-avatar.vue";
+import testLink from "./components/test-link.vue";
 </script>
 
 <style scoped>
---
+++ src/components/test-link.vue
@@ -0,0 +1,7 @@
+<template>
+    <nl-link :theme="['button']">abc</nl-link>
+    <nl-link :theme="['button', 'actived']">abc</nl-link>
+    <nl-link :theme="['button']" disabled>abc</nl-link>
+</template>
+
+<script setup></script>
