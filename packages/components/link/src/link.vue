c386e26845942d8157bae6192ac57f2c6ad4a63b
Nathan33 <Lee1928@outlook.com>
2023-11-21 20:16:08 +0800

    Optimize code

--- packages/components/link/src/link.vue
+++ packages/components/link/src/link.vue
@@ -3,8 +3,9 @@
         :is="disabled ? 'a' : tag"
         :class="classList"
         :href="disabled || !href ? undefined : href"
-        :to="to || href || undefined"
-        active-class="nl-link--actived">
+        :to="to"
+        :title="href || undefined"
+        :active-class="activeClass">
         <i v-if="icon" class="nl-link__icon" :class="icon && `iconfont ${icon}`"></i>
         <slot v-else name="icon"></slot>
         <slot></slot>
@@ -20,9 +21,12 @@ import "../style/link.css";
 defineOptions({ name: "NlLink" });
 const props = defineProps(linkProps);
 
-/**
- * Define class list
- */
+const to = computed(() => {
+    if (!props.disabled && props.tag !== "router-link") return undefined;
+    return props.to || props.href || undefined;
+});
+const activeClass = computed(() => (props.tag === "router-link" ? `nl-link--actived` : undefined));
+
 const classList = computed(() => {
     let classList = [];
     classList = ["nl-link", ...parseTheme(props.theme, "nl-link")];
