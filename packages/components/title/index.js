e1e181cbda31660b6fd5524fd5e3fc891e2a2ab6
Nathan33 <Lee1928@outlook.com>
2023-12-05 16:52:42 +0800

    暴露 nl-title* 内部组件

---
+++ packages/components/title/index.js
@@ -0,0 +1,7 @@
+import { withInstall } from "@nice-layout/utils/utils";
+
+import Title from "./src/title.vue";
+import EditableTitle from "./src/editable-title.vue";
+
+export const NlTitle = withInstall(Title);
+export const NlEditableTitle = withInstall(EditableTitle);
